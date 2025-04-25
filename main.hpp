d0f1b17c6e625cc7080b79a4073b5f0261f4155b
dgamboa4 <dgamboa4@students.avc.edu>
2025-04-25 05:16:03 +0000

    done

--- main.hpp
+++ main.hpp
@@ -4,18 +4,30 @@ using namespace std;
 
 bool isPrime(int num) {
     if (num <= 1) return false;
-    for (int i = 2; i <= sqrt(num)l i++)
-    if (num) % i == 0 return false;
+    for (int i = 2; i <= sqrt(num); i++)
+    if (num % i == 0) return false;
     return true;
 }
 
 void getTwoValues (int &begin, int &end) {
     do {
-        cin >> begin >> endl;
-    } while (begin >= endl);
+        cin >> begin >> end;
+    } while (begin >= end);
 }
 
 int getNextPrime(int begin) {
-    while (!isPrime(++begin);)
+    while (!isPrime(++begin));
     return begin;
+}
+
+int getPrevPrime(int end) {
+    while (!isPrime(--end));
+    return end;
+}
+
+int main() {
+    int begin, end;
+    getTwoValues(begin, end);
+    cout << "next prime after " << begin << " is " << getNextPrime(begin) << endl;
+    cout << "previous prime before " << end << " is " << getPrevPrime(end) << endl;
 }
