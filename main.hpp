32d36700d4f6d5b6a3945fe5debc3243beb6a339
dgamboa4 <dgamboa4@students.avc.edu>
2025-04-25 05:08:21 +0000

    save

--- main.hpp
+++ main.hpp
@@ -1,8 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i <= sqrt(num)l i++)
+    if (num) % i == 0 return false;
+    return true;
+}
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getTwoValues (int &begin, int &end) {
+    do {
+        cin >> begin >> endl;
+    } while (begin >= endl);
+}
+
+int getNextPrime(int begin) {
+    while (!isPrime(++begin);)
+    return begin;
+}
